5126b6a86a1f2017aa260ac5af6de82713a89c56
Zack Ankner <zackankner@Zacks-MacBook-Air.local>
2019-02-02 15:34:53 -0500

    add messages

--- scripts/database.js
+++ scripts/database.js
@@ -1,3 +1,14 @@
 class Database {
 
+  //Add a message to the database
+  static addMessage(message, universal, id){
+    if(universal){
+      const ref = firebase.database().ref('domains/' + domainId + '/chats/' + id + '/messages/' + message.id);
+    }
+    else{
+      const ref = firebase.database().ref('domains/' + domainId + '/chats/' + id + '/messages/' + message.id);
+    }
+    ref.set(message.serialize);
+  }
+
 }
