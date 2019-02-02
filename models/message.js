a06da46baf0c19b27f51d7c8584707c68ae74c3a
Zack Ankner <zackankner@Zacks-MacBook-Air.local>
2019-02-02 17:22:47 -0500

    add message class

---
+++ models/message.js
@@ -0,0 +1,15 @@
+class Message{
+
+  constructor(data){
+    this.messageContent = data.messageContent;
+    this.sender = data.sender;
+  }
+
+  serialize(){
+    return {
+      messageContent: this.messageContent,
+      sender: this.sender
+    };
+  }
+
+}
