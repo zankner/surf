6cfb0bfb32fb9828c61bb8e327bc0a540c4ecaf0
Ben Botvinick <benbotvinick@gmail.com>
2019-02-03 01:57:55 -0500

    formatting and small refactors

--- models/report_message.js
+++ models/report_message.js
@@ -1,20 +1,20 @@
-class ReportMessage{
+class ReportMessage {
 
-  constructor(sender, timestamp, reportContent, reportType, reported){
-    this.id = sender + timestamp.toString();
-    this.reportContent = data.reportContent;
-    this.reportType = data.reportType;
-    this.sender = data.sender;
-    this.reported = data.reported;
-  }
+	constructor(sender, timestamp, reportContent, reportType, reported) {
+		this.id = sender + timestamp.toString();
+		this.reportContent = data.reportContent;
+		this.reportType = data.reportType;
+		this.sender = data.sender;
+		this.reported = data.reported;
+	}
 
-  serialize(){
-    return {
-      id: this.id,
-      reportContent: this.reportContent,
-      reportType: this.reportType,
-      sender: this.sender,
-      reported: this.reported
-    }
-  }
+	serialize() {
+		return {
+			id: this.id,
+			reportContent: this.reportContent,
+			reportType: this.reportType,
+			sender: this.sender,
+			reported: this.reported
+		};
+	}
 }
